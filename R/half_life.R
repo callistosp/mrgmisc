718fe02ef8e523d3849bb408ebdd7379c2d91233
samc <samc@metrumrg.com>
2026-09-14 17:14:44 -0400

    Add alpha/beta half-life outputs to half_life()

--- R/half_life.R
+++ R/half_life.R
@@ -17,6 +17,7 @@ half_life <- function(cl,vc,vp,q){
   K10 <- cl/vc
   K12 <- q/vc
   K21 <- q/vp
+  L1 = ((K10+K12+K21)+((K10+K12+K21)**2-4*K10*K21)**0.5)/2
   L2 = ((K10+K12+K21)-((K10+K12+K21)**2-4*K10*K21)**0.5)/2
-  return(0.693/L2)
+  return(list(alpha=log(2)/L1, beta=log(2)/L2))
 }
